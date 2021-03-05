057bb622f80448e35a2f6ce9d93f44a941fcafa5
Vidó Márk <vidomark42@gmail.com>
2021-03-05 23:48:27 +0100

    Front end - input field update state properly Back end - parameters received properly from client side

--- src/main/java/com/trade/controllers/StockController.java
+++ src/main/java/com/trade/controllers/StockController.java
@@ -1,17 +1,38 @@
 package com.trade.controllers;
 
+import com.trade.models.Stock;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.util.MultiValueMap;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
+import java.util.Map;
 
 @CrossOrigin("*")
 @RestController
 public class StockController {
 
+    Stock stock;
+
+    @Configuration
+    @EnableWebMvc
+    public class WebConfig extends WebMvcConfigurerAdapter {
+        // this method allows cross origin requests
+        @Override
+        public void addCorsMappings(CorsRegistry registry) {
+            registry.addMapping("/**");
+        }
+    }
+
     @CrossOrigin("*")
     @GetMapping("/")
-    public String index() {
+    public String index(@RequestParam Map<String,String> requestParams) {
+
         return "Hello World";
     }
 
