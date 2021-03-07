0df96b95d5dd2d10c950f1c168cdb1b0acbcb138
Vidó Márk <vidomark42@gmail.com>
2021-03-07 17:23:47 +0100

    Back end - config for cross site request separated from controllers

--- build/resources/main/trading-app-front-end/src/components/StockResult.jsx
+++ build/resources/main/trading-app-front-end/src/components/StockResult.jsx
@@ -3,7 +3,11 @@ import { Card } from "react-bootstrap";
 
 export default function StockResult(props) {
   return props.stock.symbol != null ? (
-    <Card>Stock</Card>
+    <Card className="mt-4">
+      <div>Symbol: {props.stock.symbol}</div>
+      <div>Price: {props.stock.price}</div>
+      <div>{props.stock.message}</div>
+    </Card>
   ) : (
     <Card className="mt-4">
       <h5 className="pt-2 pb-1">Please enter a symbol and price!</h5>
